97575210e6514a7ce8359558a9622cd2613f29c5
Osei Fortune <fortune.osei@yahoo.com>
2016-05-08 00:55:43 -0400

    Added attachment handling methods

--- couchbase.android.ts
+++ couchbase.android.ts
@@ -1,5 +1,5 @@
 import applicationModule = require("application");
-
+import fs = require("file-system");
 declare var com: any;
 declare var java: any;
 
@@ -76,7 +76,7 @@ export class Couchbase {
         var query = this.database.getView(viewName).createQuery();
         var result = query.run();
         var parsedResult: Object = {};
-        while(result.hasNext()) {
+        while (result.hasNext()) {
             var row = result.next();
             parsedResult[row.getKey()] = row.getValue();
         }
@@ -130,6 +130,77 @@ export class Couchbase {
     private mapToObject(data: Object) {
         var gson = (new com.google.gson.GsonBuilder()).create();
         return JSON.parse(gson.toJson(data));
+    }
+
+    getAttachment(documentId: string, attachmentId: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            let document = this.database.getDocument(documentId);
+            let rev = document.getCurrentRevision();
+            let att = rev.getAttachment(attachmentId);
+            if (att != null) {
+                resolve(att.getContent());
+            } else {
+                reject("Sorry can't process your request");
+            }
+        })
+    }
+
+    setAttachment(documentId: string, attachmentId: string, file: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            let document = this.database.getDocument(documentId);
+            let newRev = document.getCurrentRevision().createRevision();
+            if (file.substr(0, 10).indexOf('content://') > -1) {
+                let stream = applicationModule.android.application.android.context.getContentResolver().openInputStream(file);
+                let fileExtension = android.webkit.MimeTypeMap.getFileExtensionFromUrl(file);
+                let mimeType = android.webkit.MimeTypeMap.getSingleton().getMimeTypeFromExtension(fileExtension);
+                try {
+                    newRev.setAttachment(attachmentId, mimeType, stream);
+                    newRev.save();
+                    resolve();
+                } catch (exception) {
+                    reject(exception.message);
+                }
+
+            } else if (file.substr(0, 7).indexOf('file://') > -1) {
+                let stream = applicationModule.android.application.android.context.getContentResolver().openInputStream(android.net.Uri.fromFile(file));
+                let fileExtension = android.webkit.MimeTypeMap.getFileExtensionFromUrl(file);
+                let mimeType = android.webkit.MimeTypeMap.getSingleton().getMimeTypeFromExtension(fileExtension);
+                try {
+                    newRev.setAttachment(attachmentId, mimeType, stream);
+                    newRev.save();
+                    resolve();
+                } catch (exception) {
+                    reject(exception.message);
+                }
+            } else if (file.substr(0, 2).indexOf('~/') > -1) {
+                let path = fs.path.join(fs.knownFolders.currentApp().path, file.replace('~/', ''));
+                let stream = application.android.context.getContentResolver().openInputStream(android.net.Uri.fromFile(new java.io.File(path)));
+                let fileExtension = android.webkit.MimeTypeMap.getFileExtensionFromUrl(path);
+                let mimeType = android.webkit.MimeTypeMap.getSingleton().getMimeTypeFromExtension(fileExtension);
+                try {
+                    newRev.setAttachment(attachmentId, mimeType, stream);
+                    newRev.save();
+                    resolve();
+                } catch (exception) {
+                    reject(exception.message);
+                }
+            }
+        })
+    }
+
+    removeAttachment(documentId: string, attachmentId: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            let document = this.database.getDocument(documentId);
+            let newRev = document.getCurrentRevision().createRevision();
+            try {
+                newRev.removeAttachment(attachmentId);
+                newRev.save();
+                resolve();
+            } catch (exception) {
+                reject(exception.message);
+            }
+
+        })
     }
 
 }
